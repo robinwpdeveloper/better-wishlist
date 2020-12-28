117fa9248314bf4ef5312c2f9194ec89de2c2d0f
Sabuz <naz.sabuz@gmail.com>
2020-12-28 12:16:39 +0600

    added: vendor folderx

---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '80e49d21509da302443f0043ab8f5a60a024d3da',
+    'name' => 'gadgetlife/better-wishlist',
+  ),
+  'versions' => 
+  array (
+    'gadgetlife/better-wishlist' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '80e49d21509da302443f0043ab8f5a60a024d3da',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f4ba089a5b6366e453971d3aad5fe8e897b37f41',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '39d483bdf39be819deabf04ec872eb0b2410b531',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v2.14.1',
+      'version' => '2.14.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5eb9ac5dfdd20c3f59495c22841adc5da980d312',
+    ),
+  ),
+);
