419e43922ee2f4d2d36d75a03f40d550c7522ff5
Sabuz <naz.sabuz@gmail.com>
2021-01-06 16:30:06 +0600

    updated: admin page

--- jsrc/components/tabs/style.js
+++ jsrc/components/tabs/style.js
@@ -9,30 +9,26 @@ import Color from "./../controls/color";
 class StyleSettings extends Component {
 	constructor() {
 		super(...arguments);
+	}
 
-		this.state = {
-			redirect_to_wishlist: false,
-			redirect_to_cart: false,
-			remove_from_wishlist: true,
-			show_in_loop: true,
-			position_in_loop: "after_cart",
-			position_in_single: "after_cart",
-			add_to_wishlist_text: __("Add to wishlist"),
-			add_to_cart_text: __("Add to cart"),
-			add_all_to_wishlist_text: __("Add all to cart"),
-		};
+	updateValue(value) {
+		this.props.onChange(value);
 	}
 
 	render() {
+		const { state } = this.props;
+
 		return (
 			<Fragment>
 				<h3>{__("Wishlist Button")}</h3>
 
 				<BaseControl id="wishlist-button-style" label="Style">
 					<SelectControl
-						value="default"
-						onChange={(value) => {
-							console.log(value);
+						value={state.wishlist_button_style}
+						onChange={(newValue) => {
+							this.updateValue({
+								wishlist_button_style: newValue,
+							});
 						}}
 						options={[
 							{ value: "default", label: "Theme Default" },
@@ -41,95 +37,175 @@ class StyleSettings extends Component {
 					/>
 				</BaseControl>
 
-				<BaseControl id="wishlist-button-color" label="Color">
-					<Color value="#cd2122" onChange={(value) => console.log(value)} />
-				</BaseControl>
+				{state.wishlist_button_style == "custom" && (
+					<Fragment>
+						<BaseControl id="wishlist-button-color" label="Color">
+							<Color
+								value={state.wishlist_button_color}
+								onChange={(newValue) => {
+									this.updateValue({
+										wishlist_button_color: newValue,
+									});
+								}}
+							/>
+						</BaseControl>
 
-				<BaseControl id="wishlist-button-bg-color" label="Background Color">
-					<Color value="#cd2122" onChange={(value) => console.log(value)} />
-				</BaseControl>
+						<BaseControl id="wishlist-button-bg-color" label="Background Color">
+							<Color
+								value={state.wishlist_button_background}
+								onChange={(newValue) => {
+									this.updateValue({
+										wishlist_button_background: newValue,
+									});
+								}}
+							/>
+						</BaseControl>
 
-				<BaseControl id="wishlist-button-color-hover" label="Color(hover)">
-					<Color value="#cd2122" onChange={(value) => console.log(value)} />
-				</BaseControl>
+						<BaseControl id="wishlist-button-color-hover" label="Color(hover)">
+							<Color
+								value={state.wishlist_button_hover_color}
+								onChange={(newValue) => {
+									this.updateValue({
+										wishlist_button_hover_color: newValue,
+									});
+								}}
+							/>
+						</BaseControl>
 
-				<BaseControl
-					id="wishlist-button-bg-color-hover"
-					label="Background Color(hover)"
-				>
-					<Color value="#cd2122" onChange={(value) => console.log(value)} />
-				</BaseControl>
+						<BaseControl
+							id="wishlist-button-bg-color-hover"
+							label="Background Color(hover)"
+						>
+							<Color
+								value={state.wishlist_button_hover_background}
+								onChange={(newValue) => {
+									this.updateValue({
+										wishlist_button_hover_background: newValue,
+									});
+								}}
+							/>
+						</BaseControl>
 
-				<BaseControl id="wishlist-button-border-style" label="Border">
-					<SelectControl
-						value="default"
-						onChange={(value) => {
-							console.log(value);
-						}}
-						options={[
-							{ value: "none", label: "None" },
-							{ value: "solid", label: "Solid" },
-							{ value: "dashed", label: "Dashed" },
-						]}
-					/>
-				</BaseControl>
+						<BaseControl id="wishlist-button-border-style" label="Border">
+							<SelectControl
+								value={state.wishlist_button_border_style}
+								onChange={(newValue) => {
+									this.updateValue({
+										wishlist_button_border_style: newValue,
+									});
+								}}
+								options={[
+									{ value: "none", label: "None" },
+									{ value: "solid", label: "Solid" },
+									{ value: "dashed", label: "Dashed" },
+									{ value: "dotted", label: "Dotted" },
+								]}
+							/>
+						</BaseControl>
 
-				<BaseControl id="wishlist-button-border-width" label="Border Width">
-					<RangeControl
-						value={1}
-						onChange={(columns) => console.log(columns)}
-						min={0}
-						max={10}
-					/>
-				</BaseControl>
+						{state.wishlist_button_border_style != "none" && (
+							<Fragment>
+								<BaseControl
+									id="wishlist-button-border-width"
+									label="Border Width"
+								>
+									<RangeControl
+										value={parseInt(state.wishlist_button_border_width)}
+										min={0}
+										max={10}
+										onChange={(newValue) => {
+											this.updateValue({
+												wishlist_button_border_width: newValue,
+											});
+										}}
+									/>
+								</BaseControl>
 
-				<BaseControl id="wishlist-button-border-color" label="Border Color">
-					<Color value="#cd2122" onChange={(value) => console.log(value)} />
-				</BaseControl>
+								<BaseControl
+									id="wishlist-button-border-color"
+									label="Border Color"
+								>
+									<Color
+										value={state.wishlist_button_border_color}
+										onChange={(newValue) => {
+											this.updateValue({
+												wishlist_button_border_color: newValue,
+											});
+										}}
+									/>
+								</BaseControl>
+							</Fragment>
+						)}
 
-				<BaseControl id="wishlist-button-padding-top" label="Padding Top">
-					<RangeControl
-						value={10}
-						onChange={(columns) => console.log(columns)}
-						min={0}
-						max={100}
-					/>
-				</BaseControl>
+						<BaseControl id="wishlist-button-padding-top" label="Padding Top">
+							<RangeControl
+								value={parseInt(state.wishlist_button_padding_top)}
+								min={0}
+								max={100}
+								onChange={(newValue) => {
+									this.updateValue({
+										wishlist_button_padding_top: newValue,
+									});
+								}}
+							/>
+						</BaseControl>
 
-				<BaseControl id="wishlist-button-padding-right" label="Padding Right">
-					<RangeControl
-						value={10}
-						onChange={(columns) => console.log(columns)}
-						min={0}
-						max={100}
-					/>
-				</BaseControl>
+						<BaseControl
+							id="wishlist-button-padding-right"
+							label="Padding Right"
+						>
+							<RangeControl
+								value={parseInt(state.wishlist_button_padding_right)}
+								min={0}
+								max={100}
+								onChange={(newValue) => {
+									this.updateValue({
+										wishlist_button_padding_right: newValue,
+									});
+								}}
+							/>
+						</BaseControl>
 
-				<BaseControl id="wishlist-button-padding-bottom" label="Padding Bottom">
-					<RangeControl
-						value={10}
-						onChange={(columns) => console.log(columns)}
-						min={0}
-						max={100}
-					/>
-				</BaseControl>
+						<BaseControl
+							id="wishlist-button-padding-bottom"
+							label="Padding Bottom"
+						>
+							<RangeControl
+								value={parseInt(state.wishlist_button_padding_bottom)}
+								min={0}
+								max={100}
+								onChange={(newValue) => {
+									this.updateValue({
+										wishlist_button_padding_bottom: newValue,
+									});
+								}}
+							/>
+						</BaseControl>
 
-				<BaseControl id="wishlist-button-padding-left" label="Padding Left">
-					<RangeControl
-						value={10}
-						onChange={(columns) => console.log(columns)}
-						min={0}
-						max={100}
-					/>
-				</BaseControl>
+						<BaseControl id="wishlist-button-padding-left" label="Padding Left">
+							<RangeControl
+								value={parseInt(state.wishlist_button_padding_left)}
+								min={0}
+								max={100}
+								onChange={(newValue) => {
+									this.updateValue({
+										wishlist_button_padding_left: newValue,
+									});
+								}}
+							/>
+						</BaseControl>
+					</Fragment>
+				)}
 
 				<h3>{__("Cart Button")}</h3>
 
-				<BaseControl id="cart-button-style" label="Style">
+				<BaseControl id="wishlist-button-style" label="Style">
 					<SelectControl
-						value="default"
-						onChange={(value) => {
-							console.log(value);
+						value={state.cart_button_style}
+						onChange={(newValue) => {
+							this.updateValue({
+								cart_button_style: newValue,
+							});
 						}}
 						options={[
 							{ value: "default", label: "Theme Default" },
@@ -138,71 +214,165 @@ class StyleSettings extends Component {
 					/>
 				</BaseControl>
 
-				<BaseControl id="cart-button-color" label="Color">
-					<Color value="#cd2122" onChange={(value) => console.log(value)} />
-				</BaseControl>
+				{state.cart_button_style == "custom" && (
+					<Fragment>
+						<BaseControl id="wishlist-button-color" label="Color">
+							<Color
+								value={state.cart_button_color}
+								onChange={(newValue) => {
+									this.updateValue({
+										cart_button_color: newValue,
+									});
+								}}
+							/>
+						</BaseControl>
 
-				<BaseControl id="cart-button-color-hover" label="Color(hover)">
-					<Color value="#cd2122" onChange={(value) => console.log(value)} />
-				</BaseControl>
+						<BaseControl id="wishlist-button-bg-color" label="Background Color">
+							<Color
+								value={state.cart_button_background}
+								onChange={(newValue) => {
+									this.updateValue({
+										cart_button_background: newValue,
+									});
+								}}
+							/>
+						</BaseControl>
 
-				<BaseControl id="cart-button-bg-color" label="Background Color">
-					<Color value="#cd2122" onChange={(value) => console.log(value)} />
-				</BaseControl>
+						<BaseControl id="wishlist-button-color-hover" label="Color(hover)">
+							<Color
+								value={state.cart_button_hover_color}
+								onChange={(newValue) => {
+									this.updateValue({
+										cart_button_hover_color: newValue,
+									});
+								}}
+							/>
+						</BaseControl>
 
-				<BaseControl
-					id="cart-button-bg-color-hover"
-					label="Background Color(hover)"
-				>
-					<Color value="#cd2122" onChange={(value) => console.log(value)} />
-				</BaseControl>
+						<BaseControl
+							id="wishlist-button-bg-color-hover"
+							label="Background Color(hover)"
+						>
+							<Color
+								value={state.cart_button_hover_background}
+								onChange={(newValue) => {
+									this.updateValue({
+										cart_button_hover_background: newValue,
+									});
+								}}
+							/>
+						</BaseControl>
 
-				<BaseControl id="cart-button-border-color" label="Border Color">
-					<Color value="#cd2122" onChange={(value) => console.log(value)} />
-				</BaseControl>
+						<BaseControl id="wishlist-button-border-style" label="Border">
+							<SelectControl
+								value={state.cart_button_border_style}
+								onChange={(newValue) => {
+									this.updateValue({
+										cart_button_border_style: newValue,
+									});
+								}}
+								options={[
+									{ value: "none", label: "None" },
+									{ value: "solid", label: "Solid" },
+									{ value: "dashed", label: "Dashed" },
+									{ value: "dotted", label: "Dotted" },
+								]}
+							/>
+						</BaseControl>
 
-				<BaseControl
-					id="cart-button-border-color-hover"
-					label="Border Color(hover)"
-				>
-					<Color value="#cd2122" onChange={(value) => console.log(value)} />
-				</BaseControl>
+						{state.cart_button_border_style != "none" && (
+							<Fragment>
+								<BaseControl
+									id="wishlist-button-border-width"
+									label="Border Width"
+								>
+									<RangeControl
+										value={parseInt(state.cart_button_border_width)}
+										min={0}
+										max={10}
+										onChange={(newValue) => {
+											this.updateValue({
+												cart_button_border_width: newValue,
+											});
+										}}
+									/>
+								</BaseControl>
 
-				<BaseControl id="cart-button-padding-top" label="Padding Top">
-					<RangeControl
-						value={10}
-						onChange={(columns) => console.log(columns)}
-						min={0}
-						max={100}
-					/>
-				</BaseControl>
+								<BaseControl
+									id="wishlist-button-border-color"
+									label="Border Color"
+								>
+									<Color
+										value={state.cart_button_border_color}
+										onChange={(newValue) => {
+											this.updateValue({
+												cart_button_border_color: newValue,
+											});
+										}}
+									/>
+								</BaseControl>
+							</Fragment>
+						)}
 
-				<BaseControl id="cart-button-padding-right" label="Padding Right">
-					<RangeControl
-						value={10}
-						onChange={(columns) => console.log(columns)}
-						min={0}
-						max={100}
-					/>
-				</BaseControl>
+						<BaseControl id="wishlist-button-padding-top" label="Padding Top">
+							<RangeControl
+								value={parseInt(state.cart_button_padding_top)}
+								min={0}
+								max={100}
+								onChange={(newValue) => {
+									this.updateValue({
+										cart_button_padding_top: newValue,
+									});
+								}}
+							/>
+						</BaseControl>
 
-				<BaseControl id="cart-button-padding-bottom" label="Padding Bottom">
-					<RangeControl
-						value={10}
-						onChange={(columns) => console.log(columns)}
-						min={0}
-						max={100}
-					/>
-				</BaseControl>
+						<BaseControl
+							id="wishlist-button-padding-right"
+							label="Padding Right"
+						>
+							<RangeControl
+								value={parseInt(state.cart_button_padding_right)}
+								min={0}
+								max={100}
+								onChange={(newValue) => {
+									this.updateValue({
+										cart_button_padding_right: newValue,
+									});
+								}}
+							/>
+						</BaseControl>
 
-				<BaseControl id="cart-button-padding-left" label="Padding Left">
-					<RangeControl
-						value={10}
-						onChange={(columns) => console.log(columns)}
-						min={0}
-						max={100}
-					/>
-				</BaseControl>
+						<BaseControl
+							id="wishlist-button-padding-bottom"
+							label="Padding Bottom"
+						>
+							<RangeControl
+								value={parseInt(state.cart_button_padding_bottom)}
+								min={0}
+								max={100}
+								onChange={(newValue) => {
+									this.updateValue({
+										cart_button_padding_bottom: newValue,
+									});
+								}}
+							/>
+						</BaseControl>
+
+						<BaseControl id="wishlist-button-padding-left" label="Padding Left">
+							<RangeControl
+								value={parseInt(state.cart_button_padding_left)}
+								min={0}
+								max={100}
+								onChange={(newValue) => {
+									this.updateValue({
+										cart_button_padding_left: newValue,
+									});
+								}}
+							/>
+						</BaseControl>
+					</Fragment>
+				)}
 			</Fragment>
 		);
 	}
